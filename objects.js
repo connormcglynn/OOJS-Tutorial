1f8d41c0f5d640f45fb694f44d3bfd6ae15b7222
Connor McGlynn <connorappleid@intracitygeeks.org>
2018-07-17 13:34:27 -0400

    lesson 7 / method chaining

--- objects.js
+++ objects.js
@@ -2,20 +2,27 @@ class User {
     constructor(email, name) {
         this.email = email;
         this.name = name;
+        this.score = 0;
     }
     login(){
         console.log(this.email, 'has logged in');
+        return this;
     }
     logout(){
         console.log(this.email, 'has logged out');
+        return this;
+    }
+    updateScore(){
+        this.score++;
+        console.log(this.email, 'score is now', this.score);
+        return this;
     }
 }
 
 var userOne = new User('ryu@ninjas.com', 'Ryu');
 var userTwo = new User('yoshi@mariokorp.com', 'Yoshi');
 
-userOne.login();
-userTwo.logout();
+userOne.login().updateScore().updateScore().logout();
 
 // the 'new' keyword
 // -- creates a new empty object {}
