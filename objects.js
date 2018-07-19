b2c6dad00552797d2226646f17d38b8bf9faf516
Connor McGlynn <connorappleid@intracitygeeks.org>
2018-07-19 10:36:10 -0400

    lesson 9 / constructors (under the hood)

--- objects.js
+++ objects.js
@@ -1,42 +1,14 @@
-class User {
-    constructor(email, name) {
-        this.email = email;
-        this.name = name;
-        this.score = 0;
-    }
-    login(){
+function User(email, name) { // Constructor for User class
+    this.email = email;
+    this.name = name;
+    this.online = false;
+    this.login = function(){
         console.log(this.email, 'has logged in');
-        return this;
-    }
-    logout(){
-        console.log(this.email, 'has logged out');
-        return this;
-    }
-    updateScore(){
-        this.score++;
-        console.log(this.email, 'score is now', this.score);
-        return this;
-    }
-}
-
-class Admin extends User {
-    deleteUser(user) {
-        users = users.filter(u => {
-            return u.email != user.email;
-        })
     }
 }
 
 var userOne = new User('ryu@ninjas.com', 'Ryu');
 var userTwo = new User('yoshi@mariokorp.com', 'Yoshi');
-var adminOne = new Admin('connorappleid@intracitygeeks.org', 'Connor');
 
-var users = [userOne, userTwo, adminOne];
-
-adminOne.deleteUser(userOne);
-console.log(users);
-
-// the 'new' keyword
-// -- creates a new empty object {}
-// -- sets the value of 'this' to be the new empty object
-// -- calls the constructor method
+console.log(userOne);
+userTwo.login();
